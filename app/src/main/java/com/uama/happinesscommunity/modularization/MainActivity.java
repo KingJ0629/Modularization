6820a804cf4ac132e705517becfb0bf8f28b610b
jinfeng <jinfeng@uama.com.cn>
2017-10-12 17:28:27 +0800

    arouter

--- app/src/main/java/com/uama/happinesscommunity/modularization/MainActivity.java
+++ app/src/main/java/com/uama/happinesscommunity/modularization/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 
-import com.uama.happinesscommunity.LifeFragment;
 import com.uama.happinesscommunity.life.LifeActivity;
 
 import butterknife.ButterKnife;
@@ -19,21 +17,10 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
-		
-		findViewById(R.id.layout).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				LifeFragment lifeFragment;
-				Intent i = new Intent(MainActivity.this, LifeActivity.class);
-				Log.i("msg", "转转2");
-				startActivity(i);
-			}
-		});
 	}
 	
 	@OnClick(R.id.layout)
 	public void click() {
-		LifeFragment lifeFragment;
 		Intent i = new Intent(this, LifeActivity.class);
 		Log.i("msg", "转转");
 		startActivity(i);
