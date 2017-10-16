922d4410a32bddffa1cde294ac2b2daa2055b306
jinfeng <jinfeng@uama.com.cn>
2017-10-16 17:40:22 +0800

    =.=

--- life/src/main/java/com/uama/happinesscommunity/life/LifeActivity.java
+++ life/src/main/java/com/uama/happinesscommunity/life/LifeActivity.java
@@ -1,10 +1,16 @@
 package com.uama.happinesscommunity.life;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.uama.happinesscommunity.arouter.constant.ARouterConstant;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,6 +20,8 @@ import butterknife.OnClick;
  */
 public class LifeActivity extends AppCompatActivity {
 
+	private final static String TAG = "LifeActivity.class";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,5 +39,49 @@ public class LifeActivity extends AppCompatActivity {
 	public void click2() {
 		LifeBean bean = new LifeBean("xixi");
 		ARouter.getInstance().build(ARouterConstant.LIFE_INDEX2).withObject("bean", bean).navigation();
+	}
+	
+	/**
+	 * 解决fragment onActivityResult不调用
+	 * @param requestCode
+	 * @param resultCode
+	 * @param data
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		FragmentManager fm = getSupportFragmentManager();
+		if (fm.getFragments() == null) {
+			Log.w(TAG, "Activity result fragment fragmentIndex out of range: 0x"
+					+ Integer.toHexString(requestCode));
+			return;
+		}
+		for (int i = 0; i <fm.getFragments().size() ; i++) {
+			Fragment frag = fm.getFragments().get(i);
+			if (frag == null) {
+				Log.w(TAG, "Activity result no fragment exists for fragmentIndex: 0x"
+						+ Integer.toHexString(requestCode));
+			} else {
+				handleResult(frag, requestCode, resultCode, data);
+			}
+		}
+		return;
+	}
+	
+	/**
+	 * 递归调用，对所有子Fragment生效
+	 * @param frag
+	 * @param requestCode
+	 * @param resultCode
+	 * @param data
+	 */
+	private void handleResult(Fragment frag, int requestCode, int resultCode, Intent data) {
+		frag.onActivityResult(requestCode, resultCode, data);
+		List<Fragment> frags = frag.getChildFragmentManager().getFragments();
+		if (frags != null) {
+			for (Fragment f : frags) {
+				if (f != null)
+					handleResult(f, requestCode, resultCode, data);
+			}
+		}
 	}
 }
